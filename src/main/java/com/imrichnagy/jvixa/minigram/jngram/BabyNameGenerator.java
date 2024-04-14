0841062918daa9fa9df480d25ce9f7fb48793e06
NimRo97 <nimronagy@gmail.com>
2024-04-14 20:11:00 +0200

    Generalize generator to N-gram

--- src/main/java/com/imrichnagy/jvixa/minigram/jngram/BabyNameGenerator.java
+++ src/main/java/com/imrichnagy/jvixa/minigram/jngram/BabyNameGenerator.java
@@ -1,4 +1,4 @@
-package com.imrichnagy.jvixa.minigram.jbigram;
+package com.imrichnagy.jvixa.minigram.jngram;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -7,19 +7,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static com.imrichnagy.jvixa.minigram.jbigram.NativeNgram.NAME_ALPHABET;
+import static com.imrichnagy.jvixa.minigram.jngram.NativeNgram.NAME_ALPHABET;
+import static com.imrichnagy.jvixa.minigram.jngram.NativeNgram.getPosition;
 
 public class BabyNameGenerator {
 
     private static final String FILE_PATH = "/resources/text/baby_names/";
 
-    private static final boolean WEIGHTED = true;
+    private static final int SLICE = 3;
+    private static final boolean WEIGHTED = false;
 
     private static final int FROM_YEAR = 1880; // 1880 minimum
-    private static final int TO_YEAR = 2022; // 2022 maximum
+    private static final int TO_YEAR = 1970; // 2022 maximum
 
     private static final boolean MALE = true;
     private static final boolean FEMALE = true;
+
+    private static final int NAME_COUNT = 20;
 
     public static void main(String[] args) throws IOException {
         List<String> fileNames = new ArrayList<>();
@@ -27,14 +31,14 @@ public class BabyNameGenerator {
             fileNames.add(FILE_PATH + "yob" + i + ".txt");
         }
         NativeNgram ngram = loadBigram(fileNames);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < NAME_COUNT; i++) {
             generateName(ngram);
         }
     }
 
     private static void generateName(NativeNgram bigram) {
         StringBuilder name = new StringBuilder();
-        String current = "::";
+        String current = ":".repeat(SLICE);
         while (!current.endsWith(";")) {
             char next = bigram.getNextCharacter(current);
             name.append(next);
@@ -44,20 +48,15 @@ public class BabyNameGenerator {
     }
 
     private static NativeNgram loadBigram(List<String> fileNames) throws IOException {
-        List<String> alphabet = new ArrayList<>(NAME_ALPHABET);
-        for (String c1 : NAME_ALPHABET) {
-            for (String c2 : NAME_ALPHABET) {
-                alphabet.add(c1 + c2);
-            }
-        }
-        int size = alphabet.size() * (alphabet.size() + 1);
-        long[][] frequencies = new long[size][alphabet.size()];
+        List<Character> alphabet = NAME_ALPHABET;
+        int size = alphabet.size();
+        int[][] frequencies = new int[(int) Math.pow(size, SLICE)][size];
 
         for (String fileName : fileNames) {
             try (Stream<String> stream = Files.lines(Paths.get(System.getProperty("user.dir") + fileName))) {
                 stream.forEach((line) -> {
-                    String[] parts = line.split(",");
-                    String sex = parts[1];
+                    String[] data = line.split(",");
+                    String sex = data[1];
                     if (sex.equals("M") && !MALE) {
                         return;
                     }
@@ -65,15 +64,15 @@ public class BabyNameGenerator {
                         return;
                     }
 
-                    long count = Integer.parseInt(parts[2]);
-                    String name = parts[0].toLowerCase();
+                    int count = Integer.parseInt(data[2]);
+                    String name = ":".repeat(SLICE) + data[0].toLowerCase() + ";";
 
-                    frequencies[alphabet.indexOf("::")][alphabet.indexOf(name.charAt(0) + "")] += WEIGHTED ? count : 1;
-                    frequencies[alphabet.indexOf(":" + name.charAt(0))][alphabet.indexOf(name.charAt(1) + "")]++;
-                    for (int i = 0; i < name.length() - 2; i++) {
-                        frequencies[alphabet.indexOf("" + name.charAt(i) + name.charAt(i + 1))][alphabet.indexOf(name.charAt(i + 2) + "")] += WEIGHTED ? count : 1;
+                    for (int i = 0; i < name.length() - SLICE; i++) {
+                        frequencies
+                                [getPosition(name.substring(i, i + SLICE), alphabet)]
+                                [alphabet.indexOf(name.charAt(i + SLICE))]
+                                += WEIGHTED ? count : 1;
                     }
-                    frequencies[alphabet.indexOf("" + name.charAt(name.length() - 2) + name.charAt(name.length() - 1))][alphabet.indexOf(";")] += WEIGHTED ? count : 1;
                 });
             }
         }
